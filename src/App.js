498d6b8ce73a5b8b23c563235a2237b20b9f767b
George <killyen444@gmail.com>
2023-10-11 23:17:55 +0800

    add default template to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './pages/home/Home';
+import Contact from './pages/contact/Contact';
+
 
 function App() {
   return (
-    <div>
-      Wellcome to the store!!
-    </div>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Contact/>} />
+      </Routes>
+    <Footer/>
+    </BrowserRouter>
   );
 }
 
