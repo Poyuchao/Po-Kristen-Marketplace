39e0d9d8722c15151134bd1fac30e59546458115
BubuCatt <jiayuhsu8240@gmail.com>
2023-10-18 01:04:20 +0800

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export {default as Header} from "./header/Header";
-export {default as Footer} from "./footer/Footer";
+export {default as Header} from "./Header/Header";
+export {default as Footer} from "./Footer/Footer";
 
 const navlinks = [
     {
