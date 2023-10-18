57fd4187a8a32c16196c17e2be59e75935bc5a54
BubuCatt <jiayuhsu8240@gmail.com>
2023-10-19 01:26:49 +0800

    Update App.js
    Added container to hold the input column, and stick the footer at the bottom

--- src/App.js
+++ src/App.js
@@ -8,14 +8,26 @@ import { Header,Footer } from './components';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Header/>
+    // sticky the footer at the bottom of web
+    <div className='flex flex-col min-h-screen'>
+          <BrowserRouter>
+          <Header/>
+    
+    <div className='flex-grow'>
+      {/* the main content is wrapped inside a div with the flex-grow class. This ensures it takes up all available space, pushing the footer down. */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+
+    </div>
+
     <Footer/>
     </BrowserRouter>
+
+    </div>
+
+    
   );
 }
 
