931f9c02dde87e6164189274eed422699bba2e7c
BubuCatt <jiayuhsu8240@gmail.com>
2023-10-24 02:11:51 +0800

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,8 @@
 import React,{useState} from "react";
-import navlinks from "..";  // import navlinks from index page
-import logo from '../../image/logo.png'
+import navlinks from "../index.js";  // import navlinks from index page
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter,faInstagram, faFacebookF, faGithub  } from '@fortawesome/free-brands-svg-icons';
+
 
 const Header = () => {
     // initialize open is false
@@ -11,33 +13,61 @@ const Header = () => {
     };
 
     return (
-        <div className="bg-gray-800 relative"> {/* Added relative for positioning context */}
-            <div className="mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="bg-#FAFAFA relative "> {/* Added relative for positioning context */}
+            {/* Social Icon Bar */}
+            <div className="bg-[#474747] flex justify-end items-center p-2 md:p-4 lg:p-6">
+                {/* Twitter Icon */}
+                <a href="https://twitter.com" target="#" rel="no_opener no_referrer" className="relative w-5 h-5 md:w-6 md:h-6 lg:w-8 lg:h-8 bg-[#f8f8f61f] hover:bg-gray-300 active:bg-gray-400 rounded-full mr-2 transition duration-300">
+                    <FontAwesomeIcon icon={faTwitter} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white" />
+                </a>
+                {/* FB Icon  */}
+                <a href="https://twitter.com" target="#" rel="no_opener no_referrer" className="relative w-5 h-5 md:w-6 md:h-6 lg:w-8 lg:h-8 bg-[#f8f8f61f] hover:bg-gray-300 active:bg-gray-400 rounded-full mr-2 transition duration-300">
+                    <FontAwesomeIcon icon={faFacebookF} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white" />
+                </a>
+                {/* IG Icon  */}
+                <a href="https://twitter.com" target="#" rel="no_opener no_referrer" className="relative w-5 h-5 md:w-6 md:h-6 lg:w-8 lg:h-8 bg-[#f8f8f61f] hover:bg-gray-300 active:bg-gray-400 rounded-full mr-2 transition duration-300">
+                    <FontAwesomeIcon icon={faInstagram} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white" />
+                </a>
+                {/* Github Icon  */}
+                <a href="https://twitter.com" target="#" rel="no_opener no_referrer" className="relative w-5 h-5 md:w-6 md:h-6 lg:w-8 lg:h-8 bg-[#f8f8f61f] hover:bg-gray-300 active:bg-gray-400 rounded-full mr-2 transition duration-300">
+                    <FontAwesomeIcon icon={faGithub} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white" />
+                </a>
+            </div>
+            {/* Nav Bar */}
+            
+            <div className="mx-auto px-4 sm:px-6 lg:px-8 shadow-md">
                 <div className="flex items-center justify-between h-32">
                     <div className="flex items-center">
                         {/* Logo */}
                         <img
-                            src={logo}
+                            src="/Logo_removebg.png"
                             alt="Logo"
-                            className="h-20 w-20 rounded-full"
+                            className="h-20 w-20"
                         />
                         {/* Store name */}
-                        <a href="/" className="text-white ml-3 text-2xl">
+                        <a href="/" className="text-black ml-3 text-xl">
                             Taiwan Good Stuff
                         </a>
                     </div>
                     {/* Navigation Links */}
-                    <div className="hidden md:block">
-                        <div className="ml-10 flex items-baseline space-x-4">
-                            {navlinks.map((link, index) => (
-                                <a
+                    <div className="hidden md:block flex-grow ">
+                        <div className="ml-10 flex justify-center items-baseline space-x-4">
+                            {navlinks.map((link, index) => {
+                                
+                                return(
+                                    <a
                                     key={index}
-                                    className="text-white transition-all duration-500 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-md font-medium"
+                                    className={"text-black transition-all duration-500 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-md font-medium"
+                        
+          
+                                    }
                                     href={link.link}
                                 >
                                     {link.title}
                                 </a>
-                            ))}
+                                )
+                                })}
+                            
                         </div>
                     </div>
 
@@ -48,7 +78,7 @@ const Header = () => {
                         <button
                             // This button displays the hamburger icon. When it's clicked, it calls the toggleMenu function, 
                             // effectively toggling the dropdown's visibility.
-                            className="text-white focus:outline-none"
+                            className="text-gray focus:outline-none"
                             onClick={toggleMenu} 
                         >
                             <svg
@@ -106,6 +136,10 @@ const Header = () => {
 };
 
 export default Header;
+
+
+
+
 
 
 
