cb797df45c2ca8113e6adfe88e1e6436ab6c0b03
BubuCatt <jiayuhsu8240@gmail.com>
2023-12-20 18:48:59 -0800

    add about page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,16 +1,27 @@
 import React from "react";
 
+import heroImg from "../../../src/image/aboutpage_hero.jpg";
+
+
 const About = () => {
     return (
         <div className="flex flex-col min-h-screen bg-gray-100">
             {/* Hero Section */}
-            <div className="bg-gray-400 text-white text-center p-10">
+            <div className="bg-gray-500 text-white text-center p-10">
+            
                 <h1 className="text-4xl font-bold mb-2">About Us</h1>
-                <p className="font-light text-lg">Learn more about our journey and mission</p>
+                
             </div>
+            
+            {/* Content Section with Background Image */}
+            <div className="relative flex-grow">
+            <img src={heroImg} alt="Descriptive Alt Text" className="absolute top-0 left-0 w-full h-full object-cover opacity-50 z-0"/>
+
+
+            
 
             {/* Content Section */}
-            <div className="flex-grow container mx-auto p-6">
+            <div className="container mx-auto p-6 relative z-20">
                 {/* Introduction */}
                 <section className="my-8">
                     <h2 className="text-3xl font-semibold mb-4">Our Story</h2>
@@ -18,7 +29,7 @@ const About = () => {
                     <div className="text-gray-700 text-lg">
                     <p >
                     Welcome to Taiwan Good Stuff, your passport to the delectable world of Taiwanese flavors. 
-                    <p>Explore our diverse array of savory and sweet treats and mouthwatering pastries. </p> 
+                    Explore our diverse array of savory and sweet treats and mouthwatering pastries.  
                     Discover the taste of Taiwan, one tasty bite at a time.
                     </p>
 
@@ -30,34 +41,24 @@ const About = () => {
                 <section className="my-8">
                     <h2 className="text-3xl font-semibold mb-4">Our Mission</h2>
                     <p className="text-gray-700 text-lg">
-                        Describe your mission and the values that guide your company.
+                    Taiwan Good Stuff celebrates Taiwan's dessert culture, bringing the world exquisite flavors and heartfelt stories from our rich culinary heritage, connecting people through the joy of Taiwan’s unique sweets.
                     </p>
                 </section>
 
-                {/* Team Members */}
-                <section className="my-8">
-                    <h2 className="text-3xl font-semibold mb-4">Meet the Team</h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                        {/* Team Member Card */}
-                        <div className="bg-white rounded-lg p-6 shadow-md">
-                            <img src="/team-member.jpg" alt="Team Member" className="w-full h-40 object-cover rounded-md mb-4"/>
-                            <h3 className="text-xl font-semibold mb-1">Bubu</h3>
-                            <p className="text-gray-600 text-sm">CEO & Founder</p>
-                        </div>
-                        {/* Add more team member cards here */}
-                    </div>
-                </section>
+
+                
 
                 {/* Contact or CTA Section */}
-                {/* <section className="my-8 bg-blue-500 text-white p-6 rounded-lg">
-                    <h2 className="text-3xl font-semibold mb-4">Join Our Journey</h2>
-                    <p className="font-light mb-4">
-                        A call-to-action to engage with your business or organization.
-                    </p>
-                    <button className="px-6 py-2 bg-white text-blue-500 font-bold rounded hover:bg-gray-200 transition duration-300 ease-in-out">
-                        Get Involved
+                 <section className="my-8 bg-gray-600 text-white p-6 rounded-lg">
+                    <h2 className="text-3xl font-semibold mb-4">Contact Us</h2>
+
+                    <p className="mt-2 text-white">Feel free to reach out for collaborations or feedback.</p>
+
+                    <button className="mt-4 px-6 py-2 bg-white text-gray-700 font-bold rounded hover:bg-gray-200 transition duration-300 ease-in-out">
+                        Contact Us
                     </button>
-                </section> */}
+                </section> 
+                </div>
             </div>
         </div>
     );
